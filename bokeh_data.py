9d4c0e4654a70184ae5d97bb51e071a6bdd25316
Connor <connorbrook@googlemail.com>
2021-08-08 21:58:11 +0100

    new_cases html file

--- bokeh_data.py
+++ bokeh_data.py
@@ -77,9 +77,3 @@ class covid:
         return p
 
 
-
-# endpoint = (
-#     'https://api.coronavirus.data.gov.uk/v1/data?'
-#     'filters=areaType=nation;areaName=england&'
-#     'structure={"date":"date","newCases":"newCasesByPublishDate"}'
-# )
