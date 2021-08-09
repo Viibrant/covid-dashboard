e96321b13ef5826a250c8c7dc17d60d0b98f8381
Connor <connorbrook@googlemail.com>
2021-08-09 16:53:24 +0100

    successfully got two bokeh plots on same page

--- app.py
+++ app.py
@@ -2,27 +2,24 @@ from flask import Flask, render_template
 from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
+from bokeh.layouts import gridplot
 from bokeh_data import covid
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    #return render_template('index.html')
-    return "foo"
-
-@app.route('/cases_graph')
 def bokeh():
     global dataset
-    fig = dataset.cases_graph()
+    fig1 = dataset.cases_graph()
+    fig2 = dataset.cases_graph()
     # grab the static resources
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
 
     # render template
-    script, div = components(fig)
+    script, div = components({"fig1":fig1, "fig2":fig2})
     html = render_template(
-        'new_cases.html',
+        'index.html',
         plot_script=script,
         plot_div=div,
         js_resources=js_resources,
