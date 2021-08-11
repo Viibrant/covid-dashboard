101d26a799fae540e8fd8f906700b297efc3e750
Connor <connorbrook@googlemail.com>
2021-08-11 09:48:32 +0100

    rename to make purpose clearer

--- app.py
+++ app.py
@@ -3,9 +3,29 @@ from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
 from bokeh.layouts import gridplot
-from bokeh_data import covid
+from plots import covid
 
 app = Flask(__name__)
+
+@app.route('/temp')
+def temp():
+    global dataset
+    fig1 = dataset.cases_graph()
+    fig2 = dataset.cases_graph()
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+    grid = gridplot([[fig1], [fig2]])
+    # render template
+    script, div = components(grid)
+    html = render_template(
+        'new_cases.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return html
 
 @app.route('/')
 def bokeh():
