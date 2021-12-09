7aee79dacb2723b3be3139803b23bd8abe3b78f0
Viibrant <11376396+Viibrant@users.noreply.github.com>
2021-12-09 05:41:23 +0000

    migrating to plotly

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
-
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.layouts import row
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 from plots import covid
 
 app = Flask(__name__)
@@ -17,36 +16,18 @@ def loaded():
     print(national_new_cases)
     emit(national_new_cases)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    global dataset
-    cases_plot = dataset.cases_graph()
-    cases_map = dataset.cases_map()
-    vaccines_plot = dataset.vaccines_graph()
-    vaccines_map = dataset.vaccines_map()
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
+    fig = dataset.cases_graph()
+    plotly_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("index.html", plotly_json=plotly_json)
 
-    # render template
-    script, div = components(
-        {"cases_plot": cases_plot, 
-        "cases_map": cases_map,
-        "vaccines_plot": vaccines_plot,
-        "vaccines_map": vaccines_map}
-        )
-
-    html = render_template(
-        'index.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    return html
 
 if __name__ == "__main__":
     metrics = ["newCasesBySpecimenDate", "newPeopleVaccinatedCompleteByVaccinationDate"]
-    endpoint = "https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric={metrics}&format=json".format(metrics="&metric=".join(metrics))
+    endpoint = "https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric={metrics}&format=json".format(
+        metrics="&metric=".join(metrics)
+    )
     dataset = covid(endpoint)
     socketio.run(app, debug=True)
