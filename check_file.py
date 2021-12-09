67b6f2114f425bbcba10e888864fe2f43c2bf8ec
Viibrant <11376396+Viibrant@users.noreply.github.com>
2021-12-09 05:41:05 +0000

    greatly simplified & refactored code

--- check_file.py
+++ check_file.py
@@ -1,74 +1,57 @@
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 from requests import get
 from requests.exceptions import RequestException
-from os.path import isfile, getctime
+from os.path import isfile
 from os import stat
 import time
 import json
+import logging
 import pandas as pd
 from tqdm import tqdm
 
+logging.basicConfig(level=logging.INFO)
+
+def retrieve_data(endpoint):
+    response = get(endpoint, allow_redirects=True, timeout=10, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024  # 1 Kibibyte
+    progress_bar = tqdm(total=total_size, unit="iB", unit_scale=True)
+    with open("statistics.json", "wb") as json_file:
+        for i in response.iter_content(block_size):
+            progress_bar.update(len(i))
+            json_file.write(i)
+
+def read_file():
+    with open("statistics.json") as f:
+        data = json.load(f)
+    return data
 
 def get_dataset(endpoint):
     file_exists = isfile("statistics.json")
+    empty = not (file_exists and stat("statistics.json").st_size != 0)
     retries = 1
-    latest = False
-    empty = True
 
-    if file_exists & stat("statistics.json").st_size != 0:
-        empty = False
+    if file_exists:
+        created = datetime.fromtimestamp(stat("statistics.json").st_mtime)
+        old = created == date.today()
 
-    if not empty:
-        with open("statistics.json") as json_file:
-            data = json.load(json_file)["body"]
-
-            # Read latest date from statistics.json and convert to Date object
-            latest_date = datetime.strptime(data[0]["date"], "%Y-%m-%d").date()
-
-            # Get exact creation time then parse only date
-            creation_date = getctime("statistics.json")
-            creation_date = datetime.fromtimestamp(creation_date).date()
-            # Data is always retrieved a day behind
-            #   therefore if statistics.json is up to date then
-            #       current_date == latest_date + 1 day
-            if date.today() == latest_date + timedelta(days=1):
-                print("**** Dataset is up to date")
-                # Convert dictionary to a Pandas DataFrame, far more efficient
-                return pd.DataFrame(data)
-            # Otherwise dataset itself may be outdated but retrieved today
-            else:
-                if date.today() == creation_date:
-                    print("**** Created today but outdated data")
-                    return pd.DataFrame(data)
-                else:
-                    print("**** Dataset is outdated, downloading...")
-                    latest = False
-
-    while not latest or not file_exists:
-        print("File exists:%s\nLatest version:%s" % (file_exists, latest))
-        # Attempt to retrieve COVID Statistics from NHS, waiting time grows incrementally
+    if not file_exists or empty or old:
         try:
-            response = get(endpoint, allow_redirects=True, timeout=10, stream=True)
-            total_size = int(response.headers.get("content-length", 0))
-            block_size = 1024  # 1 Kibibyte
-            progress_bar = tqdm(total=total_size, unit="iB", unit_scale=True)
-            data = b""
-            with open("statistics.json", "wb") as json_file:
-                for i in response.iter_content(block_size):
-                    progress_bar.update(len(i))
-                    json_file.write(i)
-                    data += i
-                data = json.loads(data)["body"]
-
-            return pd.DataFrame(data)
-
+            data = retrieve_data(endpoint)
         except RequestException as e:
             wait = retries * 2
             print("Error! Waiting %s secs and re-trying..." % wait)
             time.sleep(wait)
             retries += 1
             if retries == 5:
-                raise e
+                raise e        
+    data = read_file()["body"]
+    return pd.DataFrame(data)
+
+    # Check whether file was created today
+
+
+
 
 
 if __name__ == "__main__":
